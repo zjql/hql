673fb8e268036a5a50485724f9eb8c6358539dcd
张志杰 <zj6897@qq.com>
2019-09-24 16:40:23 +0800

    连接vue

--- src/main/java/com/hql/HqlApplication.java
+++ src/main/java/com/hql/HqlApplication.java
@@ -1,15 +1,22 @@
 package com.hql;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+
 
 @EnableScheduling
 @SpringBootApplication
 public class HqlApplication {
 
+    private static Logger logger = LogManager.getLogger(HqlApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(HqlApplication.class, args);
+        logger.info("hql启动成功");
     }
 
 }
--- src/main/java/com/hql/controller/UserTableController.java
+++ src/main/java/com/hql/controller/UserTableController.java
@@ -1,15 +1,19 @@
 package com.hql.controller;
 
+import com.hql.config.Result;
 import com.hql.entity.UserTable;
 import com.hql.service.UserTableService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
+import java.util.Objects;
 
 
 /**
@@ -30,5 +34,20 @@ public class UserTableController {
         logger.info(userId);
 //        return userId;
         return userTableService.queryAllUserTableId(userId);
+    }
+
+    @RequestMapping("/login")
+    public Result login(@RequestBody UserTable userTable){
+
+        // 对 html 标签进行转义，防止 XSS 攻击
+        String username = userTable.getName();
+        username = HtmlUtils.htmlEscape(username);
+        if (!Objects.equals("admin", username) || !Objects.equals("123456", userTable.getPassword())) {
+            String message = "账号密码错误";
+            System.out.println("test");
+            return new Result(400);
+        } else {
+            return new Result(200);
+        }
     }
 }
